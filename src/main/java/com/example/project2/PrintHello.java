a01b5a750760ec1bb727015c4b3a9e0dde2ce9aa
하늘 <gureumi74@naver.com>
2023-04-19 16:22:54 +0900

    feat: Hello 출력 기능 추가 - Java Project 빌드 실습

---
+++ src/main/java/com/example/project2/PrintHello.java
@@ -0,0 +1,7 @@
+package com.example.project2;
+
+public class PrintHello {
+    public void print() {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/com/example/project2/PrintHelloTest.java
@@ -0,0 +1,8 @@
+package com.example.project2;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello(); // 인스턴스화
+        printHello.print();
+    }
+}
