83a532f547d0bba189bf1e046d94a786fcd7886a
하늘 <gureumi74@naver.com>
2023-04-19 16:56:29 +0900

    feat: BufferedReader 두 숫자 더하기 - BufferedReaderTwoNumbers, BufferedReaderTwoNumbersTest 추가

---
+++ src/main/java/com/example/project2/week1/BufferedReaderTwoNumbers.java
@@ -0,0 +1,17 @@
+package com.example.project2.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderTwoNumbers {
+    public void plusTwoNums() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 와 동일
+
+        // 값을 두 개 입력받아서 더하기
+        System.out.println(br.readLine() + br.readLine());
+    }
+
+}
---
+++ src/main/java/com/example/project2/week1/BufferedReaderTwoNumbersTest.java
@@ -0,0 +1,11 @@
+package com.example.project2.week1;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderTwoNumbersTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReaderTwoNumbers brtn = new BufferedReaderTwoNumbers();
+        brtn.plusTwoNums();
+    }
+}
