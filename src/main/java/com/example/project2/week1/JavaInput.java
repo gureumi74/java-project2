bc9ff8c34db6c09bed8c200008aa24b5f47a70c4
하늘 <gureumi74@naver.com>
2023-04-19 16:41:26 +0900

    feat: InputStreamReader 두 글자 읽어오기 추가 - readTwoChars 메소드 추가

--- src/main/java/com/example/project2/week1/JavaInput.java
+++ src/main/java/com/example/project2/week1/JavaInput.java
@@ -8,4 +8,13 @@ public class JavaInput {
         InputStreamReader is = new InputStreamReader(System.in);
         System.out.println(is.read()); // 한글자씩 읽어옴
     }
+
+    public void readTwoChars() throws IOException {
+        // InputStreamReader 선언하면서 인스턴스화
+        InputStreamReader is = new InputStreamReader(System.in);
+
+        // 총 두 글자 읽어서 코드 출력
+        System.out.println(is.read());
+        System.out.println(is.read());
+    }
 }
--- src/main/java/com/example/project2/week1/JavaInputTest.java
+++ src/main/java/com/example/project2/week1/JavaInputTest.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class JavaInputTest {
     public static void main(String[] args) throws IOException {
         JavaInput javaInput = new JavaInput();
-        javaInput.readAChar();
+        javaInput.readTwoChars();
     }
 }
