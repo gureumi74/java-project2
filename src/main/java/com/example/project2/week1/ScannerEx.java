b04ee3cbe084c3a89a2f0b6dc75db93d023a3ad1
하늘 <gureumi74@naver.com>
2023-04-19 17:06:18 +0900

    feat: Scanner 두 숫자 더하기 - ScannerEx, ScannerExTest 추가

---
+++ src/main/java/com/example/project2/week1/ScannerEx.java
@@ -0,0 +1,11 @@
+package com.example.project2.week1;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public void readTwoNumbersAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+
+}
---
+++ src/main/java/com/example/project2/week1/ScannerExTest.java
@@ -0,0 +1,8 @@
+package com.example.project2.week1;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx scannerEx = new ScannerEx();
+        scannerEx.readTwoNumbersAndPlus();
+    }
+}
