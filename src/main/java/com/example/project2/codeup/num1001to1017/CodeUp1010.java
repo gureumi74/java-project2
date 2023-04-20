d7ca7abf69b6bb5ed67a82ae38395085a3758703
하늘 <gureumi74@naver.com>
2023-04-20 18:53:47 +0900

    fix: commit시에 빠진 내용 수정 - println() -> println(n) 수정

--- src/main/java/com/example/project2/codeup/num1001to1017/CodeUp1010.java
+++ src/main/java/com/example/project2/codeup/num1001to1017/CodeUp1010.java
@@ -6,6 +6,6 @@ public class CodeUp1010 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println();
+        System.out.println(n);
     }
 }
