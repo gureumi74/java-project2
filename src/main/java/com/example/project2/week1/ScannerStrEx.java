48b3a685aac464f3c56db8cc7039e443f0e13226
하늘 <gureumi74@naver.com>
2023-04-20 09:40:19 +0900

    feat: Scanner 문자 두 줄 합치기 - ScannerStrEx, ScannerStrExTest 추가

---
+++ src/main/java/com/example/project2/week1/ScannerStrEx.java
@@ -0,0 +1,10 @@
+package com.example.project2.week1;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+    public void inputStringsAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc. next() + sc.next());
+    }
+}
---
+++ src/main/java/com/example/project2/week1/ScannerStrExTest.java
@@ -0,0 +1,8 @@
+package com.example.project2.week1;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringsAndPlus();
+    }
+}
