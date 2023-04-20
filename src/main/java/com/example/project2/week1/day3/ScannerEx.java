fd8ec0dc5ccd2f4c2c54fac9b75c3e8921b57753
하늘 <gureumi74@naver.com>
2023-04-20 10:40:46 +0900

    fix: ScannerEx 안내문 추가 - ScannerEx에 입력 시 안내문 추가

--- src/main/java/com/example/project2/week1/day3/ScannerEx.java
+++ src/main/java/com/example/project2/week1/day3/ScannerEx.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ScannerEx {
     public void readTwoNumbersAndPlus() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("두 개의 숫자를 입력하세요. 각 숫자마다 입력 후 엔터");
         System.out.println(sc.nextInt() + sc.nextInt());
     }
-
 }
