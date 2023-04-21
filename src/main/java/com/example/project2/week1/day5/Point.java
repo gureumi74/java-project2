c6c79ec45d9671ef56a6dd4b075ea79512e5968a
하늘 <gureumi74@naver.com>
2023-04-21 17:42:08 +0900

    fix: Point 수정

--- src/main/java/com/example/project2/week1/day5/Point.java
+++ src/main/java/com/example/project2/week1/day5/Point.java
@@ -8,6 +8,14 @@ public class Point {
         return x == y;
     }
 
+    float getDistance(Point p) {
+        // 두 점 사이의 거리 구하기
+        int xD = p.x - this.x;
+        int yD = p.y - this.y;
+
+        return xD;
+    }
+
     public static void main(String[] args) {
         Point p = new Point();
         System.out.println(p.x);
