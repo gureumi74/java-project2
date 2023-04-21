dab688c0ae67d3052e92885955a635423489dd6b
하늘 <gureumi74@naver.com>
2023-04-21 15:14:52 +0900

    feat: User Object 생성 - User, UserTest 추가

---
+++ src/main/java/com/example/project2/week1/day5/User.java
@@ -0,0 +1,8 @@
+package com.example.project2.week1.day5;
+
+public class User {
+    // 이름, 전화번호 나이;
+    String name;
+    String phoneNum;
+    int age;
+}
---
+++ src/main/java/com/example/project2/week1/day5/UserTest.java
@@ -0,0 +1,15 @@
+package com.example.project2.week1.day5;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User Haneul = new User();
+        Haneul.name = "김하늘";
+        Haneul.phoneNum = "010-xxxx-xxxx";
+        Haneul.age = 24;
+
+        User Babo = new User();
+        Babo.name = "이바보"; // 야 진짜 아니야~
+        Babo.phoneNum = "010-1111-1111";
+        Babo.age = 22;
+    }
+}
