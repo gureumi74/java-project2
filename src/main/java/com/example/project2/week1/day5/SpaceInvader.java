62cc1d683ac0d8cf5170071c599e232f812460fd
하늘 <gureumi74@naver.com>
2023-04-21 13:23:23 +0900

    fix: 실행을 위한 main 생성 - main 추가

--- src/main/java/com/example/project2/week1/day5/SpaceInvader.java
+++ src/main/java/com/example/project2/week1/day5/SpaceInvader.java
@@ -9,4 +9,13 @@ public class SpaceInvader {
         int location = 0;
         location = 1;
     }
+
+    public static void main(String[] args) {
+        SpaceInvader si = new SpaceInvader();
+        si.moveLeft();
+        si.moveRight();
+        si.moveRight(); // 여러 번 해도 location = 1
+
+
+    }
 }
