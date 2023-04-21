a0e3af362340244c8d54cd03ee9ebb06bdf6f611
하늘 <gureumi74@naver.com>
2023-04-21 14:50:28 +0900

    feat: Point로 OOP 실습해보기 - Point, PointTest 추가, SpaceInvadersMemberVariable 수정

---
+++ src/main/java/com/example/project2/week1/day5/Point.java
@@ -0,0 +1,16 @@
+package com.example.project2.week1.day5;
+
+public class Point {
+    int x;
+    int y;
+
+    boolean isSameXy() {
+        return x == y;
+    }
+
+    public static void main(String[] args) {
+        Point p = new Point();
+        System.out.println(p.x);
+    }
+
+}
--- src/main/java/com/example/project2/week1/day5/SpaceInvadersMemberVariable.java
+++ src/main/java/com/example/project2/week1/day5/SpaceInvadersMemberVariable.java
@@ -4,11 +4,11 @@ public class SpaceInvadersMemberVariable {
     int location; // 멤버 변수
     public void moveLeft() {
         location -= 1;
-        System.out.printf("moveLeft: %d\n", location);
+//        System.out.printf("moveLeft: %d\n", location);
     }
     public void moveRight() {
         location += 1;
-        System.out.printf("moveLeft: %d\n", location);
+//        System.out.printf("moveLeft: %d\n", location);
     }
 
     public static void main(String[] args) {
@@ -20,8 +20,9 @@ public class SpaceInvadersMemberVariable {
         simv.moveRight();
         simv.moveRight();
 
-
-
+        System.out.printf("최종 위치: %d\n", simv.location);
+        simv.moveLeft();
+        System.out.printf("최종 위치: %d\n", simv.location);
     }
 
 }
