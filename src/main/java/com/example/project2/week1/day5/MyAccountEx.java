c1f448067a91d5ce7fd2268158ec44d8c7202ce6
하늘 <gureumi74@naver.com>
2023-04-21 11:18:52 +0900

    feat: 상수 선언 및 사용 - MyAccountEx 추가

---
+++ src/main/java/com/example/project2/week1/day5/MyAccountEx.java
@@ -0,0 +1,11 @@
+package com.example.project2.week1.day5;
+
+public class MyAccountEx {
+    public static void main(String[] args) {
+        int sumOfAccount = 1000;
+        System.out.printf("내 통장엔 %d원이 들어있습니다.\n", sumOfAccount);
+
+        sumOfAccount = 2000;
+        System.out.printf("내 통장엔 %d원이 들어있습니다.\n", sumOfAccount);
+    }
+}
