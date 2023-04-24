cb6132e9abe841a8cb28ce5c7692fa6af242014b
하늘 <gureumi74@naver.com>
2023-04-24 15:32:24 +0900

    feat: Boolean 실습 - BooleanEx 추가

---
+++ src/main/java/com/example/project2/week2/day6/BooleanEx.java
@@ -0,0 +1,12 @@
+package com.example.project2.week2.day6;
+
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean flag1 = false;
+        boolean flag2 = true;
+        boolean isPaymentSuccess = true;
+        boolean isBalanceSufficient = false;
+
+        System.out.println("isBalanceSufficient = " + isBalanceSufficient);
+    }
+}
