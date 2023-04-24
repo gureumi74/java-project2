b5c498ee8b14f1786e60f8ec9d292952b2378397
하늘 <gureumi74@naver.com>
2023-04-24 15:40:21 +0900

    feat: CodeUp1049-1052 풀이

---
+++ src/main/java/com/example/project2/codeup/num1049to1058/CodeUp1049.java
@@ -0,0 +1,15 @@
+package com.example.project2.codeup.num1049to1058;
+
+import java.util.Scanner;
+
+public class CodeUp1049 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        if (sc.nextInt() > sc.nextInt()) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
