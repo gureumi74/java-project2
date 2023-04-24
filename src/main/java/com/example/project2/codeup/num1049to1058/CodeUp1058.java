9650bb432e44725f2fc034a23c12262159a20e48
하늘 <gureumi74@naver.com>
2023-04-24 19:33:24 +0900

    feat: CodeUp1054-1058 풀이

---
+++ src/main/java/com/example/project2/codeup/num1049to1058/CodeUp1058.java
@@ -0,0 +1,14 @@
+package com.example.project2.codeup.num1049to1058;
+
+import java.util.Scanner;
+
+public class CodeUp1058 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if(sc.nextInt() == 0 & sc.nextInt() == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
