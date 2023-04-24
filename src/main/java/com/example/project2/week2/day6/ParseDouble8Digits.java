4d33054f06285f81fe1f0f6c7c6c8663b0c36b3e
하늘 <gureumi74@naver.com>
2023-04-24 15:32:18 +0900

    feat: String을 Double형으로 변환 - ParseDouble8Digits 추가

---
+++ src/main/java/com/example/project2/week2/day6/ParseDouble8Digits.java
@@ -0,0 +1,10 @@
+package com.example.project2.week2.day6;
+
+public class ParseDouble8Digits {
+    public static void main(String[] args) {
+        String val1 = "1.11111111";
+        String val2 = "2.11111111";
+        double result2 = Double.parseDouble(val1) + Double.parseDouble(val2);
+        System.out.println(result2);
+    }
+}
