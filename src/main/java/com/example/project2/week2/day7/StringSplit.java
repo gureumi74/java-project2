22b02ca45b8361aedd685cbad7b066d969517754
하늘 <gureumi74@naver.com>
2023-04-25 14:12:27 +0900

    feat: String .split 실습 - StringSplit 추가

---
+++ src/main/java/com/example/project2/week2/day7/StringSplit.java
@@ -0,0 +1,11 @@
+package com.example.project2.week2.day7;
+
+public class StringSplit {
+    public static void main(String[] args) {
+        String str = "10 3 19 28 7 488";
+        String[] strArr = str.split(" ");
+        for(int i = 0; i < strArr.length; i++) {
+            System.out.println(strArr[i]);
+        }
+    }
+}
