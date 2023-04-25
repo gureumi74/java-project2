fa64b9a086e927a8aec0813d09b3c0f5f86213bd
하늘 <gureumi74@naver.com>
2023-04-25 11:15:24 +0900

    fead: CodeUp1020 .substring을 이용해서 풀어보기

--- src/main/java/com/example/project2/codeup/num1018to1027/CodeUp1020.java
+++ src/main/java/com/example/project2/codeup/num1018to1027/CodeUp1020.java
@@ -6,13 +6,17 @@ public class CodeUp1020 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+//        String cmd = sc.nextLine();
+//        String number = "";
+//
+//        for(String s : cmd.split("-")) {
+//            number += s;
+//        }
+//
+//        System.out.printf(number);
+
+        // .substring 을 이용해서 풀기
         String cmd = sc.nextLine();
-        String number = "";
-
-        for(String s : cmd.split("-")) {
-            number += s;
-        }
-
-        System.out.printf(number);
+        System.out.println(cmd.substring(0, 6) + cmd.substring(7));
     }
 }
