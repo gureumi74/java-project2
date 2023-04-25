8e09c06513ba99fd7479170c3b81f7317b34e371
하늘 <gureumi74@naver.com>
2023-04-25 11:16:19 +0900

    feat: String .substring 사용 실습 - StringSubstring 추가

---
+++ src/main/java/com/example/project2/week2/day7/StringSubstring.java
@@ -0,0 +1,9 @@
+package com.example.project2.week2.day7;
+
+public class StringSubstring {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(str.substring(0, 2)); // 0번인덱스부터 2번 인덱스전까지 출력
+        System.out.println(str.substring(1, 3)); // el 출력
+    }
+}
