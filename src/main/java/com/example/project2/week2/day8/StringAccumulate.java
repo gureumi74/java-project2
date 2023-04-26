a41938dc8e18b4c85949b30e050ecc0ae2bfddb7
하늘 <gureumi74@naver.com>
2023-04-26 15:59:17 +0900

    feat: 문자열 누적하기 - StringAccumulate 추가

---
+++ src/main/java/com/example/project2/week2/day8/StringAccumulate.java
@@ -0,0 +1,12 @@
+package com.example.project2.week2.day8;
+
+public class StringAccumulate {
+    public static void main(String[] args) {
+        String answer = "";
+        System.out.println(answer);
+        answer += "쿵";
+        System.out.println(answer);
+        answer += "짝";
+        System.out.println(answer);
+    }
+}
