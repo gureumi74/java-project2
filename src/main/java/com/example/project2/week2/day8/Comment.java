166eeacee8e0d9a2fc546efeb3a25b24643e3ef8
하늘 <gureumi74@naver.com>
2023-04-26 10:32:53 +0900

    feat: 배열을 멤버 변수로 선언하기 실습2 - Post, Comment 추가

---
+++ src/main/java/com/example/project2/week2/day8/Comment.java
@@ -0,0 +1,5 @@
+package com.example.project2.week2.day8;
+
+public class Comment {
+    String content;
+}
---
+++ src/main/java/com/example/project2/week2/day8/Post.java
@@ -0,0 +1,7 @@
+package com.example.project2.week2.day8;
+
+public class Post {
+    String title;
+    String content;
+    Comment[] comments;
+}
