8d2589577aa48bc549d632013a996fd16c0078da
하늘 <gureumi74@naver.com>
2023-04-26 13:38:06 +0900

    feat: 배열의 길이 출력 - ArrLength 추가

---
+++ src/main/java/com/example/project2/week2/day8/ArrLength.java
@@ -0,0 +1,8 @@
+package com.example.project2.week2.day8;
+
+public class ArrLength {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        System.out.println(arr.length);
+    }
+}
