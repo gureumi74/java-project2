307daab80193b75ab578d0a8833920344836c03b
하늘 <gureumi74@naver.com>
2023-04-26 10:33:17 +0900

    feat: 2차원 배열 생성 - TenxTen 추가

---
+++ src/main/java/com/example/project2/week2/day8/TenxTen.java
@@ -0,0 +1,13 @@
+package com.example.project2.week2.day8;
+
+import java.util.Arrays;
+
+public class TenxTen {
+    public static void main(String[] args) {
+        int[][] arr10x10 = new int[10][10];
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println(Arrays.toString(arr10x10[i]));
+        }
+    }
+}
