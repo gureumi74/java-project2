349cd0de6887812c8465dad139e10bbd5bab9181
하늘 <gureumi74@naver.com>
2023-04-27 10:17:26 +0900

    feat: 배열의 인덱스를 증감연산자로 사용하기 - IncreaseFirst 추가

---
+++ src/main/java/com/example/project2/week2/day9/IncreaseFirst.java
@@ -0,0 +1,10 @@
+package com.example.project2.week2.day9;
+
+public class IncreaseFirst {
+    public static void main(String[] args) {
+        int idx = 0;
+        int[] arr = {2, 1, 7, 9};
+        System.out.println(arr[idx++]); // 결과값 : 2
+        System.out.println(arr[idx++]); // 결과값 : 1
+    }
+}
