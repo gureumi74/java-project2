959ddc5e621d94e788a2fa23939236829edac404
하늘 <gureumi74@naver.com>
2023-04-27 10:16:53 +0900

    feat: 증감연산자 i++와 ++i의 차이점 - IncreaseOperator 추가

---
+++ src/main/java/com/example/project2/week2/day9/IncreaseOperator.java
@@ -0,0 +1,21 @@
+package com.example.project2.week2.day9;
+
+public class IncreaseOperator {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i++); // 결과값 : 10
+        System.out.println(i); // 결과값 : 11
+
+        int j = 10;
+        System.out.println(++j); // 결과값 : 11
+        System.out.println(j); // 결과값 : 11
+
+        int x = 10;
+        System.out.println(x--); // 결과값 : 10
+        System.out.println(x); // 결과값 : 9
+
+        int y = 10;
+        System.out.println(--y); // 결과값 : 9
+        System.out.println(y); // 결과값 : 9
+    }
+}
