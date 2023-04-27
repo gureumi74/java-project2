c74e7a7982f19538ee1add8174ed69196c499fc0
하늘 <gureumi74@naver.com>
2023-04-27 16:29:14 +0900

    feat: Shift 연산자 사용 - BitOperatorEx 추가

---
+++ src/main/java/com/example/project2/week2/day9/BitOperatorEx.java
@@ -0,0 +1,13 @@
+package com.example.project2.week2.day9;
+
+public class BitOperatorEx {
+    public static void main(String[] args) {
+        int val1 = 1;
+        System.out.println(val1 << 1); // 2배 2
+        System.out.println(val1 << 2); // 2배 4
+
+        int val2 = 32;
+        System.out.println(val2 >> 1); // /2 16
+        System.out.println(val2 >> 2); // /2 8
+    }
+}
