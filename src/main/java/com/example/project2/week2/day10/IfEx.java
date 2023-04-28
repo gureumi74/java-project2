d554d40b05029c9ed3a18df35146c1a9537e0f44
하늘 <gureumi74@naver.com>
2023-04-28 09:29:48 +0900

    feat: if문 선언 및 사용 - IfEx 추가

---
+++ src/main/java/com/example/project2/week2/day10/IfEx.java
@@ -0,0 +1,9 @@
+package com.example.project2.week2.day10;
+
+public class IfEx {
+    public static void main(String[] args) {
+        if(true) {
+            System.out.println("정상 실행됩니다.");
+        }
+    }
+}
