29d007c61a75320c98d0feb9b506982352eceb45
하늘 <gureumi74@naver.com>
2023-04-28 09:48:22 +0900

    feat: [프로그래머스] 콜라츠 추측 풀이

---
+++ src/main/java/com/example/project2/plustest/Programmers2.java
@@ -0,0 +1,28 @@
+package com.example.project2.plustest;
+
+public class Programmers2 {
+    public static int solution(int num) {
+        int answer = 0;
+        while (num > 1) {
+            if(num % 2 == 0) {
+                num /= 2;
+            } else {
+                num *= 3;
+                num += 1;
+            }
+
+            answer++;
+            if (answer >= 500) {
+                break;
+            }
+        }
+
+        return num == 1 ? answer : -1;
+
+    }
+    public static void main(String[] args) {
+        int num = 626331;
+        int answer = solution(num);
+        System.out.println(answer);
+    }
+}
