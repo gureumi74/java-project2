7956200996760e602bd64be75abefd894c62d054
하늘 <gureumi74@naver.com>
2023-05-01 15:50:25 +0900

    feat: [프로그래머스] 배열 만들기2 int 로만 풀기

---
+++ src/main/java/com/example/project2/week3/day11/Programmers2.java
@@ -0,0 +1,39 @@
+package com.example.project2.week3.day11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Programmers2 {
+    public static int[] solution(int l, int r) {
+        // int로만 해보기
+        List list = new ArrayList();
+        for(int i = l; i <= r; i++) {
+            int num = i;
+            boolean flag = true;
+            while (num > 0) {
+                int check = num % 10;
+                if(check != 0 && check != 5) {
+                    flag = false;
+                    break;
+                }
+                num /= 10;
+            }
+
+            if(flag) {
+                list.add(i);
+            }
+        }
+
+        if(list.size() == 0) {
+            list.add(-1);
+        }
+
+        return list.stream().mapToInt(x -> (int) x).toArray();
+    }
+    public static void main(String[] args) {
+        int l = 5;
+        int r = 555;
+        System.out.println(solution(l, r));
+
+    }
+}
