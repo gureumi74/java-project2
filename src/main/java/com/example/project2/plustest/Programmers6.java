071ad3c8b8f7da1ba5e6b492b56635b27f740eb6
하늘 <gureumi74@naver.com>
2023-05-01 11:08:45 +0900

    feat: [프로그래머스] 이어 붙인 수 int 만으로 풀이

--- src/main/java/com/example/project2/plustest/Programmers6.java
+++ src/main/java/com/example/project2/plustest/Programmers6.java
@@ -15,8 +15,23 @@ public class Programmers6 {
 
         return Integer.parseInt(odd) + Integer.parseInt(even);
     }
+    public static int solution2(int[] num_list) {
+        // int 만으로 풀기
+        int odd = 0;
+        int even = 0;
+
+        for(int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                even = even * 10 + num_list[i];
+            } else {
+                odd = odd * 10 + num_list[i];
+            }
+        }
+
+        return odd + even;
+    }
     public static void main(String[] args) {
         int[] num_list = {3, 4, 5, 2, 1};
-        System.out.println(solution(num_list));
+        System.out.println(solution2(num_list));
     }
 }
