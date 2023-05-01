e7363912e60bae823198288f664f63c0336810ac
하늘 <gureumi74@naver.com>
2023-05-01 10:36:29 +0900

    feat: For문 예제 - ForLoopForward, ForLoopForwardStep, ForLoopReverse 추가

---
+++ src/main/java/com/example/project2/week3/day11/ForLoopForward.java
@@ -0,0 +1,9 @@
+package com.example.project2.week3.day11;
+
+public class ForLoopForward {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ src/main/java/com/example/project2/week3/day11/ForLoopReverse.java
@@ -0,0 +1,9 @@
+package com.example.project2.week3.day11;
+
+public class ForLoopReverse {
+    public static void main(String[] args) {
+        for(int i = 5; i > 0; i--) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
