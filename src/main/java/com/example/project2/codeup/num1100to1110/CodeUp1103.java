a1433b6eb1c64347880a9b02643bdb42a85d2c1d
하늘 <gureumi74@naver.com>
2023-05-01 10:36:47 +0900

    feat: CodeUp1103, CodeUp1106 풀이

---
+++ src/main/java/com/example/project2/codeup/num1100to1110/CodeUp1103.java
@@ -0,0 +1,7 @@
+package com.example.project2.codeup.num1100to1110;
+
+public class CodeUp1103 {
+    public static void main(String[] args) {
+        System.out.println("\"c:\\test\"");
+    }
+}
---
+++ src/main/java/com/example/project2/codeup/num1100to1110/CodeUp1106.java
@@ -0,0 +1,8 @@
+package com.example.project2.codeup.num1100to1110;
+
+public class CodeUp1106 {
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
