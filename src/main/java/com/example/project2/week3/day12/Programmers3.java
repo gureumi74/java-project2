aad571f2145ad8162156d7c932627c6953655f3d
하늘 <gureumi74@naver.com>
2023-05-02 13:49:46 +0900

    feat: [프로그래머스] 수열과 구간 쿼리 2-4 풀이

---
+++ src/main/java/com/example/project2/week3/day12/Programmers3.java
@@ -0,0 +1,21 @@
+package com.example.project2.week3.day12;
+
+import java.util.Arrays;
+
+public class Programmers3 {
+    public static int[] solution(int[] arr, int[][] queries) {
+        // 수열과 구간 쿼리 3
+        for(int i = 0; i < queries.length; i++) {
+            int tmp = arr[queries[i][0]];
+            arr[queries[i][0]] = arr[queries[i][1]];
+            arr[queries[i][1]] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 3, 4};
+        int[][] queries = {{0, 3}, {1, 2}, {1, 4}};
+        System.out.println(Arrays.toString(solution(arr, queries)));
+    }
+}
