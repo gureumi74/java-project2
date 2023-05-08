a47adaac7ef1ed98149cd50496def75f9f5c1029
하늘 <gureumi74@naver.com>
2023-05-08 10:06:47 +0900

    feat: 피라미드 별 만들기

---
+++ src/main/java/com/example/project2/week4/day15/forExam.java
@@ -0,0 +1,16 @@
+package com.example.project2.week4.day15;
+
+import java.util.Scanner;
+
+public class forExam {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int count = 0;
+        for(int i = 1; i <= input; i++) {
+            System.out.print(" ".repeat(input - i));
+            System.out.println("*".repeat(i + count));
+            count++;
+        }
+    }
+}
